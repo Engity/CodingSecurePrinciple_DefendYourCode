9e224b12dce643e013f5775dcc0dd19a1880c810
Tinh Diep <tdiep00@uw.edu>
2023-05-05 14:27:57 -0700

    Updated checkName() in Verifier.java Updated checkPassword() in Verifier.java Updated checkInt() in Verifier.java

--- Java_Solution/Verifier.java
+++ Java_Solution/Verifier.java
@@ -1,11 +1,26 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Verifier{
+    private static Pattern myPattern;
+    private static Matcher myMatcher;
+
     /**
      * Check whether a string is an integer
      * @param s, contains the number to check
      * @return a number if s is an int; null if it is not
      */
     public static Integer checkInt(String s){
-        return null;
+        Integer num = null;
+        try {
+            num = Integer.parseInt(s);
+            if ((num & 0x80000000) != 0 && (num & 0x7FFFFFFF) != 0) {
+                return null;
+            }
+        } catch (NumberFormatException e) {
+            return null;
+        }
+        return num;
     }
 
     /**
@@ -14,6 +29,14 @@ public class Verifier{
      * @return true if s is a name, false if it is not
      */
     public static boolean checkName(String s){
-        return true;
+        myPattern = Pattern.compile("^[a-zA-Z]{1,50}$");
+        myMatcher = myPattern.matcher(s);
+        return myMatcher.find();
+    }
+
+    public static boolean checkPassword(String s) {
+        myPattern = Pattern.compile("^[^~)('!*<>:;,?\"|/]+\\.txt$");
+        myMatcher = myPattern.matcher(s);
+        return myMatcher.find();
     }
 }
