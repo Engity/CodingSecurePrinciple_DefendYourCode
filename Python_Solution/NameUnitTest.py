94711be398427c68fa32b877b05d712446db0a11
meyww <129892986+meyww@users.noreply.github.com>
2023-05-07 09:10:34 -0700

    update test

---
+++ Python_Solution/NameUnitTest.py
@@ -0,0 +1,52 @@
+import unittest
+import Verifier
+
+class VerifierNameTest(unittest.TestCase):
+    def test_ValidName(self):
+        self.assertEqual(Verifier.Verifier.verifyName("Toan"), "Toan",
+                        'Valid Name Test Fails')
+    
+    def test_InvalidName(self):
+        self.assertEqual(Verifier.Verifier.verifyName("Toan123"), False,
+                        'Invalid Name Test Fails')
+    
+    def test_EmptyName(self):
+        self.assertEqual(Verifier.Verifier.verifyName(""), False,
+                        'Empty Name Test Fails')
+    
+    def test_ShortName(self):
+        self.assertEqual(Verifier.Verifier.verifyName("T"), False,
+                        'Short Name Test Fails')
+    
+    def test_LongName(self):
+        self.assertEqual(Verifier.Verifier.verifyName("ToanNguyenTran"), False,
+                        'Long Name Test Fails')
+    
+    def test_NameWithSpaces(self):
+        self.assertEqual(Verifier.Verifier.verifyName("Toan Nguyen"), False,
+                        'Name With Spaces Test Fails')
+    
+    def test_NameWithSpecialChars(self):
+        self.assertEqual(Verifier.Verifier.verifyName("J@ne"), False,
+                        'Name With Special Chars Test Fails')
+                        
+                        
+    def test_NameWithLowercase(self):
+        self.assertEqual(Verifier.Verifier.verifyName("toan"), "toan",
+                        'Lowercase Name Test Fails')
+                        
+    def test_NameWithMixedCase(self):
+        self.assertEqual(Verifier.Verifier.verifyName("ToAn"), False,
+                        'Mixed Case Name Test Fails')
+                        
+    def test_NameWithAccentedChars(self):
+        self.assertEqual(Verifier.Verifier.verifyName("André"), "André",
+                        'Accented Name Test Fails')
+                        
+    def test_NameWithNumbers(self):
+        self.assertEqual(Verifier.Verifier.verifyName("Toan123"), False,
+                        'Name With Numbers Test Fails')
+                        
+    def test_NameWithSpecialCharsAndNumbers(self):
+        self.assertEqual(Verifier.Verifier.verifyName("Toan#123"), False,
+                        'Name With Special Chars and Numbers Test Fails')
---
+++ Python_Solution/TestRunner.py
@@ -0,0 +1,10 @@
+import unittest
+def suite():
+    suite = unittest.TestSuite()
+    for all_test_suite in unittest.defaultTestLoader.discover('', pattern='*Test.py'):
+        for test_suite in all_test_suite:
+            suite.addTests(test_suite)
+    return suite
+
+runner = unittest.TextTestRunner()
+runner.run(suite())
