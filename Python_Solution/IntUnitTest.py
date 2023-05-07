50f4891da79f22b523ca006f11a84aff80b3ba78
meyww <129892986+meyww@users.noreply.github.com>
2023-05-07 09:11:28 -0700

    update
    update

---
+++ Python_Solution/IntUnitTest.py
@@ -0,0 +1,45 @@
+import unittest
+import Verifier
+
+class VerifierIntTest(unittest.TestCase):
+    def test_ValidInt_Positive(self):
+        self.assertEqual(Verifier.Verifier.verifyInt("123"), 123,
+                         'Valid positive int string test fails')
+
+    def test_ValidInt_Zero(self):
+        self.assertEqual(Verifier.Verifier.verifyInt("0"), 0,
+                         'Valid zero int string test fails')
+
+    def test_ValidInt_Min(self):
+        self.assertEqual(Verifier.Verifier.verifyInt(str(-2**31)), -2147483648,
+                         'Valid minimum int value test fails')
+
+    def test_ValidInt_Max(self):
+        self.assertEqual(Verifier.Verifier.verifyInt(str(2**31 - 1)), 2147483647,
+                         'Valid maximum int value test fails')
+
+    def test_ValidInt_Whitespace(self):
+        self.assertEqual(Verifier.Verifier.verifyInt("  123  "), 123,
+                         'Valid int string with whitespace test fails')
+
+    def test_InvalidInt_Positive(self):
+        self.assertEqual(Verifier.Verifier.verifyInt("+123"), False,
+                         'Invalid positive int string test fails')
+
+    def test_InvalidInt_TooLarge(self):
+        self.assertEqual(Verifier.Verifier.verifyInt(str(2**31)), False,
+                         'Invalid int value greater than maximum test fails')
+
+    def test_InvalidInt_TooSmall(self):
+        self.assertEqual(Verifier.Verifier.verifyInt(str(-2**31 - 1)), False,
+                         'Invalid int value less than minimum test fails')
+
+    def test_InvalidInt_Float(self):
+        self.assertEqual(Verifier.Verifier.verifyInt("123.45"), False,
+                         'Invalid float string test fails')
+
+    def test_InvalidInt_Alpha(self):
+        self.assertEqual(Verifier.Verifier.verifyInt("123abc"), False,
+                         'Invalid alpha-numeric string test fails')
+
+
