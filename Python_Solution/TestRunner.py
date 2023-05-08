6d80404b2a38e2aa174f97b1b0aa36e85b0bb826
Engity <ngtoan1107@gmail.com>
2023-05-08 14:06:20 -0700

    Update Pattern for TestRunner

--- Python_Solution/TestRunner.py
+++ Python_Solution/TestRunner.py
@@ -2,7 +2,7 @@
 import unittest
 def suite():
     suite = unittest.TestSuite()
-    for all_test_suite in unittest.defaultTestLoader.discover('', pattern ='*test.py'):
+    for all_test_suite in unittest.defaultTestLoader.discover('', pattern ='*[Tt][eE][sS][Tt].[pP][yY]'):
         for test_suite in all_test_suite:
             suite.addTests(test_suite)
     return suite
