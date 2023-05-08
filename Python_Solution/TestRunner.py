21a016f209fea912d4b9d1542f753061a56e0960
Engity <ngtoan1107@gmail.com>
2023-05-08 14:22:50 -0700

    Added more comments for Test Runners

--- Python_Solution/TestRunner.py
+++ Python_Solution/TestRunner.py
@@ -1,8 +1,10 @@
 #author Toan Nguyen
 import unittest
+import os
 def suite():
     suite = unittest.TestSuite()
-    #Running all tests with the surfix "test.py"
+    #Current working directory has to be Python_Solution to run the tests
+    #Running all tests with the surfix "test.py" 
     for all_test_suite in unittest.defaultTestLoader.discover('', pattern ='*[Tt][eE][sS][Tt].[pP][yY]'):
         for test_suite in all_test_suite:
             suite.addTests(test_suite)
