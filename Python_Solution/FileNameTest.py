afd315629ba20ede8097c81714e6e08b59ced3b9
kokinh11 <tommydiep2311@yahoo.com>
2023-05-08 20:21:10 -0700

    Fixed FileNameTest.py

--- Python_Solution/FileNameTest.py
+++ Python_Solution/FileNameTest.py
@@ -4,45 +4,45 @@ import Verifier
 
 class FileNameTest(unittest.TestCase):
     def test_ValidFileName00(self):
-        password = "am-.txt"
-        self.assertTrue(Verifier.Verifier.verifyPassword(password), 'Valid password test fails')
+        file = "am-.txt"
+        self.assertTrue(Verifier.Verifier.verifyFileName(file), 'Valid file\'s name test fails')
 
     def test_ValidFileName01(self):
-        password = "a_b.txt"
-        self.assertTrue(Verifier.Verifier.verifyPassword(password), 'Valid password test fails')
+        file = "a_b.txt"
+        self.assertTrue(Verifier.Verifier.verifyFileName(file), 'Valid file\'s name test fails')
 
     def test_ValidFileName02(self):
-        password = "a_b.yoyoyo.txt"
-        self.assertTrue(Verifier.Verifier.verifyPassword(password), 'Valid password test fails')    
+        file = "a_b.yoyo.txt"
+        self.assertTrue(Verifier.Verifier.verifyFileName(file), 'Valid file\'s name test fails')    
 
     def test_InvalidFileName00(self):
-        password = "file*mey.txt"
-        self.assertFalse(Verifier.Verifier.verifyPassword(password), 'Valid password test fails')
+        file = "file*mey.txt"
+        self.assertFalse(Verifier.Verifier.verifyFileName(file), 'Valid file\'s name test fails')
 
     def test_InvalidFileName01(self):
-        password = "example@file.txt"
-        self.assertFalse(Verifier.Verifier.verifyPassword(password), 'Valid password test fails')
+        file = "example@file.txt"
+        self.assertFalse(Verifier.Verifier.verifyFileName(file), 'Valid file\'s name test fails')
 
     def test_InvalidFileName02(self):
-        password = "123ab?.txt"
-        self.assertFalse(Verifier.Verifier.verifyPassword(password), 'Valid password test fails')    
+        file = "123ab?.txt"
+        self.assertFalse(Verifier.Verifier.verifyFileName(file), 'Valid file\'s name test fails')    
 
     def test_FileWithNoExtension(self):
-        password = "toannguyen"
-        self.assertFalse(Verifier.Verifier.verifyPassword(password), 'Valid password test fails')
+        file = "toannguyen"
+        self.assertFalse(Verifier.Verifier.verifyFileName(file), 'Valid file\'s name test fails')
 
     def test_NoFileNameInputButHasEnding(self):
-        password = ".txt"
-        self.assertFalse(Verifier.Verifier.verifyPassword(password), 'Valid password test fails')
+        file = ".txt"
+        self.assertFalse(Verifier.Verifier.verifyFileName(file), 'Valid file\'s name test fails')
 
     def test_InvalidFileType00(self):
-        password = "file.text"
-        self.assertFalse(Verifier.Verifier.verifyPassword(password), 'Valid password test fails')
+        file = "file.text"
+        self.assertFalse(Verifier.Verifier.verifyFileName(file), 'Valid file\'s name test fails')
 
     def test_InvalidFileType01(self):
-        password = "file.docs"
-        self.assertTrue(Verifier.Verifier.verifyPassword(password), 'Valid password test fails')
+        file = "file.docs"
+        self.assertFalse(Verifier.Verifier.verifyFileName(file), 'Valid file\'s name test fails')
 
     def test_InvalidFileType02(self):
-        password = "file.tx" * 20
-        self.assertFalse(Verifier.Verifier.verifyPassword(password), 'Valid password test fails')
+        file = "file.tx" * 20
+        self.assertFalse(Verifier.Verifier.verifyFileName(file), 'Valid file\'s name test fails')
